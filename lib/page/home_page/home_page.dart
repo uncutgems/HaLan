fc8041fb50da7920459aebe8933abfc8f58b3642
Hoang Phuc <abchoangphuc@gmail.com>
2020-10-08 21:57:55 +0700

    check point

--- lib/page/home_page/home_page.dart
+++ lib/page/home_page/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:avwidget/av_button_widget.dart';
 import 'package:avwidget/avwidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -21,7 +22,7 @@ class _HomePageState extends State<HomePage> {
         leading: IconButton(
           icon: SvgPicture.asset(
             'assets/myicon.svg',
-            height: AppSize.getWidth(context, 16),
+            height: AppSize.getWidth(context, 12),
             width: AppSize.getWidth(context, 16),
           ),
           onPressed: () {},
@@ -38,13 +39,60 @@ class _HomePageState extends State<HomePage> {
               .copyWith(fontWeight: FontWeight.w600),
         ),
         centerTitle: true,
-        actions:<Widget> [
-          Stack(
-            children: <Widget>[
-              IconButton(icon: null, onPressed: null),
-            ],
-          ),
+        actions: <Widget>[
+          IconButton(
+              icon: SvgPicture.asset(
+                'assets/bell.svg',
+                height: AppSize.getWidth(context, 19),
+                width: AppSize.getWidth(context, 16),
+              ),
+              onPressed: () {}),
         ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(AppSize.getHeight(context, 16)),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: AVButton(
+                    leadingIcon: IconButton(
+                        icon: SvgPicture.asset(
+                          'assets/bus.svg',
+                          height: AppSize.getWidth(context, 16),
+                          width: AppSize.getWidth(context, 30),
+                        ),
+                        onPressed: () {}),
+                    title: 'Đặt xe',
+                    width: AppSize.getWidth(context, 163),
+                    height: AppSize.getWidth(context, 64),
+                    color: AVColor.orange100,
+                    onPressed: () {},
+                  ),
+                ),
+                Container(width: AppSize.getWidth(context, 16),),
+                Expanded(
+                  child: AVButton(
+
+                    leadingIcon: IconButton(
+                        icon: SvgPicture.asset(
+                          'assets/taxi.svg',
+                          height: AppSize.getWidth(context, 16),
+                          width: AppSize.getWidth(context, 30),
+                        ),
+                        onPressed: () {}),
+                    title: 'Đặt taxi',
+                    width: AppSize.getWidth(context, 163),
+                    height: AppSize.getWidth(context, 64),
+                    color: AVColor.orange100,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
