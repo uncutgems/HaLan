10ab6c757fd124beace5d23e13df16087ae2d383
skye241 <van.dambich24@gmail.com>
2020-10-08 22:52:46 +0700

    layout màn hình buslist

--- lib/pages/default_page.dart
+++ lib/pages/default_page.dart
@@ -1,4 +1,3 @@
-import 'package:avwidget/cus_text_form_field_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:halan/base/color.dart';
 
@@ -7,37 +6,8 @@ class DefaultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-    return Scaffold(
-        backgroundColor: AppColor.backgroundColor,
-        body: Form(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: CustomerTextFormFieldWidget(
-                  title: 'hello',
-                  isRequired: true,
-                  keyboardType: TextInputType.number,
-                  textEditingController: TextEditingController(),
-                  validator: (String value) {
-                    if (value.isEmpty) {
-                      return 'Vui lòng điền mật khẩu';
-                    }
-                    return null;
-                  },
-
-                ),
-              ),
-              RaisedButton(
-                child: Text('Nhấn'),
-                onPressed: () {
-                    if (_formKey.currentState.validate()) {
-
-                  }
-                },
-              ),
-            ],
-          ),
-        ));
+    return const Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+    );
   }
 }
