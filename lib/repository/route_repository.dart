48d8683d98793a73c40420b15a4874a1917c5973
HoangPhuc1999 <54959333+HoangPhuc1999@users.noreply.github.com>
2020-10-09 10:23:59 +0700

    check point

--- lib/repository/route_repository.dart
+++ lib/repository/route_repository.dart
@@ -1,23 +1,26 @@
-
 import 'package:halan/base/api_handler.dart';
 import 'package:halan/base/constant.dart';
 import 'package:halan/base/url.dart';
 import 'package:halan/model/entity.dart';
 
-class RouteRepository{
-  Future<List<RouteEntity>> getAllRoutes() async{
+class RouteRepository {
+  Future<List<RouteEntity>> getAllRoutes() async {
     final List<RouteEntity> routeList = <RouteEntity>[];
-    final AVResponse response = await callPOST(path: URL.getRouteList, body: <String,dynamic>{});
-    if(response.isOK){
-      response.response[Constant.result].forEach((final dynamic itemJson){
-        routeList.add(RouteEntity.fromMap(itemJson as Map<String,dynamic>));
+    final AVResponse response =
+        await callPOST(path: URL.getRouteList, body: <String, dynamic>{});
+    if (response.isOK) {
+      response.response[Constant.result].forEach((final dynamic itemJson) {
+        routeList.add(RouteEntity.fromMap(itemJson as Map<String, dynamic>));
       });
       print(routeList.length);
       return routeList;
-    }
-    else{
+    } else {
       throw APIException(response);
     }
-
   }
+
+
+
+
+
 }
