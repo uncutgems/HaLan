cfa394ac237ab148db20fd06438f1ccbe9df67ae
HoangPhuc1999 <54959333+HoangPhuc1999@users.noreply.github.com>
2020-10-12 13:44:09 +0700

    check point

--- lib/page/bus_booking/bus_booking_page.dart
+++ lib/page/bus_booking/bus_booking_page.dart
@@ -61,10 +61,13 @@ class _BusBookingPageState extends State<BusBookingPage> {
       ),
       body: ListView(
         children: <Widget>[
-          Container(height: AppSize.getHeight(context, 8),),
-
+          Container(
+            height: AppSize.getHeight(context, 8),
+          ),
           mainScreen(context),
-          Container(height: AppSize.getHeight(context, 16),),
+          Container(
+            height: AppSize.getHeight(context, 16),
+          ),
           PopUpWidget(),
           PopularRoute(),
         ],
@@ -74,8 +77,9 @@ class _BusBookingPageState extends State<BusBookingPage> {
 
   Widget mainScreen(BuildContext context) {
     return Padding(
-      padding:  EdgeInsets.only(left: AppSize.getWidth(context, 16),
-      right: AppSize.getWidth(context, 16)),
+      padding: EdgeInsets.only(
+          left: AppSize.getWidth(context, 16),
+          right: AppSize.getWidth(context, 16)),
       child: Column(
         children: <Widget>[
           pickLocation(
